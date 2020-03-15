6bd478bb5eebb54294570dde73a45d66b0c700e5
Callum <callum.epeel@gmail.com>
2020-03-15 21:21:51 +0800

    Setup react router. Created home component. Discussed cock.

---
+++ src/client/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from "./Home";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+   <div>
+     <Router>
+        <Switch>
+            <Route exact path="/home">
+              <Home />
+            </Route>
+        </Switch>
+      </Router>
+   </div>
+  );
+}
+
+export default App;
