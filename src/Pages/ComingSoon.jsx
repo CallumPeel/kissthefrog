0f656cc4e87392f06a4cdceecc5ba1e6c99055f7
Callum <callum.epeel@gmail.com>
2020-10-23 23:48:16 +0800

    Added imagecards and flexyboi

--- src/Pages/ComingSoon.jsx
+++ src/Pages/ComingSoon.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withGetScreen } from 'react-getscreen';
 import '../client/sass/coming-soon.scss';
+import { ImageCard } from './../components/ImageCard';
 
 class ComingSoon extends React.Component {
     constructor(props){
@@ -17,6 +18,7 @@ class ComingSoon extends React.Component {
                     <div class="main-heading-container">
                         <div className="main-heading-font-style main-heading-mobile main-heading-mobile-h1">Kiss the Frog</div>
                         <div className="main-heading-font-style main-heading-mobile main-heading-mobile-h2">Dolls Clothing</div>
+                        <div className="description-tagline-mobile description-tagline">Clothing for your 18 inch dolls</div>
                     </div>      
                 </div>
             );
@@ -30,6 +32,18 @@ class ComingSoon extends React.Component {
             <div class="main-heading-container">
                 <div className="main-heading-font-style main-heading main-heading-h1">Kiss the Frog</div>
                 <div className="main-heading-font-style main-heading main-heading-h2">Dolls Clothing</div>
+                <div className="description-tagline">Clothing for your 18 inch dolls</div>
+                <div style={{
+                    display: "flex", 
+                    flexDirection: "row", 
+                    flexWrap: "nowrap",
+                    justifyContent: "space-evenly",
+                    marginTop: "50px"
+                    }}>
+                    <ImageCard fileName='images/Doll1.jpg' fileNameAltText='doll'/>
+                    <ImageCard fileName='images/Doll2.jpg' fileNameAltText='doll2'/>
+                    <ImageCard fileName='images/Doll3.jpg' fileNameAltText='doll3'/>
+                </div>
             </div>      
          </div>
         );
